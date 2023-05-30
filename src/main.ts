159598ecdf647925990808d327d6cb7ba50eb512
jaime rey <jaimereycasado@gmail.com>
2023-05-30 02:10:35 +0200

    Primer. first commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+import { Observable, Observer, Subject } from 'rxjs';
+function recieveEvents(observable: Observable<string>) {
+  observable.subscribe({
+    next: (str) => {
+      console.log(`Event received: ${str}`);
+    },
+    complete: () => console.log('Sequence ended'),
+  });
+}
+function sendEvents(observer: Observer<string>) {
+  let count = 5;
+  for (let i = 0; i < count; i++) {
+    observer.next(`${i + 1} of ${count}`);
+  }
+  observer.complete();
+}
+let subject = new Subject<string>();
+recieveEvents(subject);
+sendEvents(subject);
